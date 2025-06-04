f2524c2b1bcfb84c0bc24b549f071aebe7f84ec4
Hoa Pham <hoaphamxuan2002@gmail.com>
2025-06-05 00:41:19 +0700

    Routes and Controllers

---
+++ blog/src/app/controllers/NewsController.js
@@ -0,0 +1,15 @@
+
+class Newcontroller {
+
+    // [GET] /news
+    index(req, res) {
+        res.render('news');
+    }
+
+    // [GET] /news/:slug
+    show(req, res) {
+        res.send('News Detail');
+    }
+}
+
+module.exports = new Newcontroller;
--- blog/src/index.js
+++ blog/src/index.js
@@ -6,6 +6,8 @@ const exphbs = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({
@@ -23,18 +25,8 @@ app.engine('hbs', exphbs.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources\\views'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-})
-
-app.get('/news', (req, res) => {
-    res.render('news');
-})
-
-app.get('/search', (req, res) => {
-    res.render('search');
-})
-
+// Routes init
+route(app);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
